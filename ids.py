a2b60912db9dfd7e4d32683a320e03c304bcc5b7
jaredbrungard <jared.brungard@gmail.com>
2025-08-30 18:03:17 -0600

    ids, get same results and bfs

--- ids.py
+++ ids.py
@@ -1,7 +1,8 @@
 import collections
 
 def valid_states(state):
-    left,right = state
+   
+    left, right = state
     if "Farmer" not in left and (("Wolf" in left and "Goat" in left) or ("Goat" in left and "Cabbage" in left)):
         return False
     if "Farmer" not in right and (("Wolf" in right and "Goat" in right) or ("Goat" in right and "Cabbage" in right)):
@@ -16,47 +17,50 @@ def next_states(state):
             if item != "Farmer":
                 new_left = left - {item, "Farmer"}
                 new_right = right | {item, "Farmer"}
-                possible_states.append((new_left, new_right))
+                possible_states.append((frozenset(new_left), frozenset(new_right)))
         new_left = left - {"Farmer"}
         new_right = right | {"Farmer"}
         possible_states.append((frozenset(new_left), frozenset(new_right)))
     else:
-        #this is how the farmer goes with an item on the boat
         for item in right:
             if item != "Farmer":
                 new_right = right - {item, "Farmer"}
                 new_left = left | {item, "Farmer"}
                 possible_states.append((frozenset(new_left), frozenset(new_right)))
-        #this is how the farmer goes alone w/o and item on the boat
         new_right = right - {"Farmer"}
         new_left = left | {"Farmer"}
         possible_states.append((frozenset(new_left), frozenset(new_right)))
 
     return [s for s in possible_states if valid_states(s)]
 
-def bfs(start, goal):
-    frontier = collections.deque([(start, [start])])
-    visited = set()
-    visited.add(start)
+def dls(state, goal, path, limit):
+    if state == goal:
+        return path
 
+    for next in next_states(state):
+        if next not in path:
+            result = dls(next, goal, path + [next], limit - 1)
+            if result is not None:
+                return result
+    return None # this is where we would return if we get no result before the limit
 
-    while frontier:
-        current_state, path = frontier.popleft()
-        if current_state == goal:
-            return path
-        for state in next_states(current_state):
-            if state not in visited:
-                visited.add(state)
-                frontier.append((state, path + [state]))
-    return None
+def ids(start, goal):
+    depth = 0
+    while True:
+        result = dls(start, goal, [start], depth)
+        if result is not None:
+            return result
+        depth += 1
 
 if __name__ == "__main__":
     #start = (frozenset({"Farmer", "Wolf", "Goat", "Cabbage"}), frozenset())
     start= (frozenset({"Farmer", "Wolf", "Goat"}), frozenset({"Cabbage"}))
     goal = (frozenset(), frozenset({"Farmer", "Wolf", "Goat", "Cabbage"}))
-    solution = bfs(start, goal)
+    solution = ids(start, goal)
+    
     if solution:
-        for step in solution:
-            print((set(step[0]), set(step[1])))
+        print("\nSolution Found!")
+        for i, step in enumerate(solution):
+            print(f"Step {i}: ( {set(step[0])}, {set(step[1])} )")
     else:
         print("No solution found.")
